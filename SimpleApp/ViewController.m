55ba44f15b10ca9e7fe4160f97f2efd3c571e135
kusebingtang <kusebintang@163.com>
2019-04-25 11:01:20 +0800

    hello world Demo

--- SimpleApp/ViewController.m
+++ SimpleApp/ViewController.m
@@ -17,7 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Do any additional setup after loading the view, typically from a nib.
+      UILabel* label = [[UILabel alloc]init];
+     //这一句创建了一个静态文本控件，未指定内容、大小和位置
+      label.text=@"hello world";
+      //"@"的作用是把一个c风格的字符串"hello world"包装成一个NSString对象
+     [label sizeToFit];
+      //让静态文本的大小自适应内容
+      label.center =self.view.center;
+     //让静态文本居于整个视图的中央
+     [self.view addSubview:label];
+      //将静态文本添加到主视图中
 }
-
 
 @end
